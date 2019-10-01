aec5d45e46d1efdd7fb2844e980447c7584f727c
Erica Norris <norrise120@gmail.com>
2019-09-30 21:07:49 -0700

    implemented min_heap methods

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -17,7 +17,14 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    node = HeapNode.new(key, value)
+
+    if @store.empty?
+      @store.push(node)
+    else
+      @store.push(node)
+      heap_up(@store.length - 1)
+    end
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +32,12 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+    return nil if @store.empty?
+
+    swap(0, @store.length - 1)
+    removed = @store.pop
+    heap_down(0)
+    return removed.value
   end
 
 
@@ -47,7 +59,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return @store.length == 0
   end
 
   private
@@ -58,14 +70,33 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+    return if @store[index].nil? || index == 0
+
+    parent_index = (index - 1) / 2
+
+    if @store[parent_index].key > @store[index].key
+     swap(index, parent_index)
+     heap_up(parent_index)
+   end
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+    left = (index * 2) + 1
+    right = (index * 2) + 2
+
+    if !@store[left] && !@store[right]
+      return 
+    elsif !@store[right]
+      min = left
+    else
+      min = @store[left].key <= @store[right].key ? left : right
+    end
+
+    swap(index, min)
+    heap_down(min)
   end
 
   # If you want a swap method... you're welcome
